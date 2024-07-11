eff6fedfc0ef83c94578e1a51d23822d4ed97b3d
unknown <105841112721@student.unimuh.ac.id>
2024-07-12 01:11:57 +0800

    tugas 2

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 const App = () => {
   return (
-    <View style= {{flex: 1}}>
-      <View style= {{flex: 1,flexDirection: 'row'}}>
-   <View style= {{flex: 1,backgroundColor:'green'}}></View>
-   <View style= {{flex: 1,backgroundColor:'blue'}}></View>
-    </View>
-    <View style= {{flex: 1,flexDirection: 'colum'}}>
-   <View style= {{flex: 1,backgroundColor:'red'}}></View>
-   <View style= {{flex: 1,backgroundColor:'yellow'}}></View>
-   <View style= {{flex: 1,backgroundColor:'green'}}></View>
-    </View>
-
+    <View style={styles.container}>
+      
+      <View style={styles.buttonContainer}>
+        <View style={[styles.button, { backgroundColor: 'red' }]}>
+          <Text style={styles.buttonText}>Login</Text>
+        </View>
+        <View style={[styles.button, { backgroundColor: 'blue' }]}>
+          <Text style={styles.buttonText}>Register</Text>
+        </View>
+      </View>
     </View>
   );
-}
-export default App
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  storeImage: {
+    width: 200, 
+    height: 100,
+    marginBottom: 20, 
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
